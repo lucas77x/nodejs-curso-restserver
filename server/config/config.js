3fd5b3dddffc55c46e02560e2fbf5e4d9a686511
Lucas Serrey <lucas.serrey@trenesargentinos.gob.ar>
2019-12-10 12:39:25 -0300

    Fix

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // =========================
 let urlDB;
 
-if( 0 )
+if (process.env.NODE_ENV === 'dev' )
 {
     urlDB = 'mongodb://localhost:27017/cursonode';
 }
